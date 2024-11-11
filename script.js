fb57cf08cf59f77267493054af1dd8e497a072e6
beibarys <tvbeiba@gmail.com>
2024-11-11 10:19:14 +0300

    exam done

---
+++ script.js
@@ -0,0 +1,18 @@
+
+    var san1 = prompt ("санды жаз");
+    var san2 = prompt ("екынщы санды жаз");
+    var san3 = prompt ("ушыншы санды жаз");
+    var kop
+    var az
+    var jauab
+    if (san1 > san2 || san1 > san3) {
+        san1 = kop
+    }    else if (san1 < san2 || san1 < san2 ) {
+        san1 = az
+    }    else if (san2 > san3 || san2 > san3) {
+        san2 = kop
+    }   else if (san2 < san3 || san2 < san3) {
+        san2=az
+    }
+
+
---
+++ task1.js
@@ -0,0 +1,13 @@
+ var seanons = prompt ( "1-12 санды енгыз" )
+
+ if (seanons == 3 || seanons == 4 || seanons == 5 )
+ alert("жаз")
+     else if (seanons == 6 || seanons == 7 || seanons == 8) {
+         alert("куз");   
+     }
+     else if (seanons == 9 || seanons == 10 || seanons == 11 ) {
+         alert("кыс");
+     }
+     else if (seanons == 12 || seanons == 1 || seanons == 2) {
+         alert("коктем")
+     }
